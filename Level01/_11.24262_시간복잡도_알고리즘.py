aecf33d8f02999326ebb521103fc30c7ea7c3cc0
pshyun123 <pshyun7204@gmail.com>
2024-03-08 17:10:01 +0900

    24262

---
+++ Level01/_11.24262_시간복잡도_알고리즘.py
@@ -0,0 +1,16 @@
+#입력의 크기 n이 주어지면 MenOfPassion 알고리즘 수행 시간을 예제 출력과 같은 방식으로 출력해보자.
+
+# MenOfPassion 알고리즘은 다음과 같다.
+# MenOfPassion(A[], n) {
+#     i = ⌊n / 2⌋;
+#     return A[i]; # 코드1
+# }
+
+#1
+print(1)
+print(0)
+
+
+#2
+input()
+print(1,0,sep='\n')
