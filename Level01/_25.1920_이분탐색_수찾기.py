67ffe4970359ede97cccc748fdcbcd6a0578cb67
pshyun123 <pshyun7204@gmail.com>
2024-03-20 23:41:17 +0900

    1920.이분탐색방법 추가

--- Level01/_25.1920_이분탐색_수찾기.py
+++ Level01/_25.1920_이분탐색_수찾기.py
@@ -42,3 +42,54 @@ arr = list(map(int, input().split()))
 
 for num in arr:				# arr의 각 원소별로 탐색
     print(1) if num in A else print(0)	# num이 A 안에 있으면 1, 없으면 0 출력
+
+
+# 방법3. 이분탐색- 첫번째는 런타임 에러/ 더 효율적인 코드(이분탐색 중에서는)
+from sys import stdin, stdout
+n = stdin.readline()
+N = sorted(map(int,stdin.readline().split()))
+m = stdin.readline()
+M = map(int, stdin.readline().split())
+
+def binary(l, N, start, end):
+    if start > end:
+        return 0
+    m = (start+end)//2
+    if l == N[m]:
+        return 1
+    elif l < N[m]:
+        return binary(l, N, start, m-1)
+    else:
+        return binary(l, N, m+1, end)
+
+for l in M:
+    start = 0
+    end = len(N)-1
+    print(binary(l,N,start,end))
+
+
+
+# 방법4. 이분탐색 - bisect_left() 함수 사용
+from bisect import bisect_left
+
+# 리스트 A의 길이와 요소들 입력받기
+N = int(input())
+A = sorted(map(int, input().split()))
+
+# 리스트 arr의 길이와 요소들 입력받기
+M = int(input())
+arr = list(map(int, input().split()))
+
+# 리스트 A에서 이분 탐색으로 숫자가 존재하는지 확인하는 함수
+def binary_search(num):
+    index = bisect_left(A, num)
+    if index < len(A) and A[index] == num: # num 이랑 작거나 같으면 리스트 안에 존재, 아니면 false 반환
+        return True
+    return False
+
+# arr의 각 원소별로 이분 탐색을 수행하여 결과 출력
+for num in arr:
+    if binary_search(num):
+        print(1)
+    else:
+        print(0)
