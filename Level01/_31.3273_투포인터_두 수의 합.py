c0baff10b6bd2a4a87a9549be0343ee96c6e72ea
pshyun123 <pshyun7204@gmail.com>
2024-03-29 16:58:37 +0900

    3273.주석추가

--- Level01/_31.3273_투포인터_두 수의 합.py
+++ Level01/_31.3273_투포인터_두 수의 합.py
@@ -8,19 +8,41 @@
 
 import sys
 
+# 입력으로 리스트의 길이를 받음
 n = int(input())
+
+# 리스트의 숫자들을 입력받아 정렬된 리스트로 저장
 numbers = sorted(list(map(int, sys.stdin.readline().split())))
+
+# 두 수를 더하여 만들고자 하는 값
 x = int(input())
 
+# 결과를 저장할 변수 초기화
 answer = 0
-left, right = 0, n-1 # 왼쪽, 오른쪽
+
+# 리스트의 가장 작은 값과 가장 큰 값을 가리키는 포인터 초기화
+left, right = 0, n - 1
+
+# left가 right보다 작은 동안 반복
 while left < right:
+    # 현재 left와 right가 가리키는 두 수의 합 계산
     temp = numbers[left] + numbers[right]
+
+    # 두 수의 합이 목표값과 같으면
     if temp == x:
+        # 경우의 수를 하나 증가시키고 left 포인터를 오른쪽으로 이동
         answer += 1
         left += 1
+
+    # 두 수의 합이 목표값보다 작으면
     elif temp < x:
+        # left 포인터를 오른쪽으로 이동하여 두 번째로 작은 수를 더함
         left += 1
+
+    # 두 수의 합이 목표값보다 크면
     else:
+        # right 포인터를 왼쪽으로 이동하여 큰 수를 줄임
         right -= 1
+
+# 결과 출력
 print(answer)
