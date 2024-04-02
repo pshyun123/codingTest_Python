dbf7e6a153f610bd5c99925c92555a9cb581b1cd
pshyun123 <pshyun7204@gmail.com>
2024-04-02 15:18:13 +0900

    11725.트리 방법 2개 추가

--- Level01/_33.11725_트리_트리의부모찾기.py
+++ Level01/_33.11725_트리_트리의부모찾기.py
@@ -4,11 +4,7 @@
 # 첫째 줄에 노드의 개수 N (2 ≤ N ≤ 100,000)이 주어진다.
 # 둘째 줄부터 N-1개의 줄에 트리 상에서 연결된 두 정점이 주어진다.
 
-
-# DFS 방식
-
-
-# BFS 방식
+# 방법1. BFS 방식
 import sys
 from collections import deque
 
@@ -35,3 +31,65 @@ while q:
             q.append(v)
 
 print('\n'.join(map(str, parent[2:])))
+
+
+
+
+# 방법2. DFS 방식(Stack)
+from collections import deque
+import sys
+input=sys.stdin.readline
+
+
+N=int(input())
+visited=[False]*(N+1)
+answer=[0]*(N+1)
+E=[[] for _ in range(N+1)]
+for i in range(N-1):
+    S,D=map(int,input().split())
+    E[S].append(D)
+    E[D].append(S)
+
+#DFS with Stack
+def dfs(E,v,visited):
+    visited[v]=True
+    stack=deque([v])
+    while stack:
+        x=stack.pop()
+        for i in E[x]:
+            if not visited[i]:
+                stack.append(i)
+                visited[i]=True
+                answer[i]=x
+dfs(E,1,visited)
+for i in range(2,N+1):
+        print(answer[i])
+
+# 방법3. DFS 방식(재귀)
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(1000000)
+
+N = int(input())
+visited = [False] * (N + 1)
+answer = [0] * (N + 1)
+E = [[] for _ in range(N + 1)]
+for i in range(N - 1):
+    S, D = map(int, input().split())
+    E[S].append(D)
+    E[D].append(S)
+
+
+def dfs(E, v, visited):
+    visited[v] = True
+    for i in E[v]:
+        if not visited[i]:
+            answer[i] = v
+            dfs(E, i, visited)
+
+
+dfs(E, 1, visited)
+
+for i in range(2, N + 1):
+    print(answer[i])
