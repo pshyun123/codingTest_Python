fd347761cdb1ef5006682b9f86a75f05a34b5cfd
pshyun123 <pshyun7204@gmail.com>
2024-04-09 21:49:52 +0900

    10430

--- 입출력과 사칙연산/_09.10430_나머지.py
+++ 입출력과 사칙연산/_09.10430_나머지.py
@@ -2,6 +2,7 @@
 # (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
 # 세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
 
+# 방법1
 a, b, c = input().split()
 a = int(a)
 b = int(b)
@@ -11,3 +12,8 @@ print((a+b)%c)
 print(((a%c)+(b%c))%c)
 print((a*b)%c)
 print(((a%c)*(b%c))%c)
+
+
+# 방법2
+A,B,C = map(int,input().split())
+print((A+B)%C, ((A%C)+(B%C))%C, (A*B)%C, ((A%C)*(B%C))%C, sep='\n')
