c53fa401886d7640eaefba6cac1f477f5d0b9610
pshyun123 <pshyun7204@gmail.com>
2024-04-10 22:14:49 +0900

    11382 풀이추가

--- 입출력과 사칙연산/_11.11382 꼬마정민.py
+++ 입출력과 사칙연산/_11.11382 꼬마정민.py
@@ -1,16 +1,19 @@
 # 꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!
 
-# 모범 답안
+# 방법1
 a, b, c = input().split()
-
 a = int(a)
 b = int(b)
 c = int(c)
-
 print(a + b + c)
 
-
+# 방법2
 a, b, c = map(int, input().split())
 print(a+b+c)
 
+# 방법3
+ls = list(map(int, input().split()))
+print(sum(ls))
 
+# 방법4
+print(sum(list(map(int, input().split()))))
